7457c208e74fefd7827dbe14ce5852a5927e689e
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2024-07-22 23:26:25 +0300

    add slot to button

--- src/components/IButton/IButton.vue
+++ src/components/IButton/IButton.vue
@@ -1,5 +1,6 @@
 <template>
   <button class="bg-[#FFA279] rounded-[14px] py-3 px-10 text-white font-bold -tracking-wider">
-    Click me!
+    <div v-if="$slots.default">It's default</div>
+    <slot>Click me!</slot>
   </button>
 </template>
---
+++ src/views/HomepageView.vue
@@ -0,0 +1,23 @@
+<script setup>
+import IButton from '../components/IButton/IButton.vue'
+</script>
+
+<template>
+  <main class="flex h-screen">
+    <section class="flex-1 flex justify-center items-center px-5 bg-primary">
+      <div class="text-white text-center">
+        <img src="../assets/img/map-pin.svg" alt="" class="inline mb-6" />
+        <h1 class="font-bold text-4xl mb-7">IT traveler</h1>
+        <p class="leading-6 mb-11">
+          A simple and convenient web application that will allow you to mark your favourite places,
+          as well as those you would really like to visit. So don't hesitate and try it for
+          yourself.
+        </p>
+        <IButton>Start</IButton>
+      </div>
+    </section>
+    <section>
+      <img src="../assets/img/static-map.png" alt="" class="h-full w-full object-cover" />
+    </section>
+  </main>
+</template>
