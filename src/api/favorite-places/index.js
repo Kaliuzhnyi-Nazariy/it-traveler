7798bdf73714d24d548007039f9fa6b701c486c0
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2024-08-23 23:53:01 +0300

    update aoi operations

--- src/api/favorite-places/index.js
+++ src/api/favorite-places/index.js
@@ -1,19 +1,27 @@
 import { clientFetch } from '../clientFetch'
 
-const BASE_PLACES_URL = '/points'
+const BASE_PLACES_URL = '/places'
 
 export const getFavoritePlaces = () => {
-  return clientFetch.get(BASE_PLACES_URL)
+  return clientFetch.get(BASE_PLACES_URL).then(({ data }) =>
+    data.map((place) => ({
+      ...place,
+      id: place._id
+    }))
+  )
 }
 
 export const addFavoritePlaces = (body) => {
-  return clientFetch.get(BASE_PLACES_URL, body)
+  return clientFetch.post(BASE_PLACES_URL, body)
 }
 
 export const updateFavoritePlaces = (body) => {
+  Reflect.deleteProperty(body, '_id')
+  Reflect.deleteProperty(body, 'owner')
+  Reflect.deleteProperty(body, '__v')
   return clientFetch.put(BASE_PLACES_URL, body)
 }
 
-export const deleteFavoritePlaces = (body) => {
-  return clientFetch.get(BASE_PLACES_URL, body)
+export const deleteFavoritePlaces = (id) => {
+  return clientFetch.delete(`${BASE_PLACES_URL}/${id}`)
 }
