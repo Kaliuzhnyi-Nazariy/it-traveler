cb050f0960320ec5c5ad70d87d5e838c2e71d2e1
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2024-08-24 00:25:08 +0300

    add emits and img props + logic what to show fallbackPhoto or props.img

--- src/components/FavoritePlace/FavoritePlace.vue
+++ src/components/FavoritePlace/FavoritePlace.vue
@@ -2,6 +2,7 @@
 import DeleteIcon from './DeleteIcon.vue'
 import EditIcon from './EditIcon.vue'
 import FavoritePlaceIconButton from './FavoritePlaceIconButton.vue'
+import fallbackPhoto from '../../assets/img/no-image.png'
 
 const props = defineProps({
   title: {
@@ -18,20 +19,26 @@ const props = defineProps({
     type: Boolean
   }
 })
+
+const emit = defineEmits(['edit', 'delete'])
 </script>
 
 <template>
   <section class="text-gray mb-6 last:mb-0">
     <div class="flex gap-4">
-      <img class="w-[76px] h-[76px] shrink-0" :src="props.img" alt="" />
+      <img
+        class="w-[76px] h-[76px] shrink-0"
+        :src="props.img.length === 0 ? fallbackPhoto : props.img"
+        alt=""
+      />
       <div class="w-full">
         <div class="flex justify-between items-center mb-2">
           <h2 class="font-bold text-sm text-[#2C2C2C]">{{ props.title }}</h2>
           <div class="flex gap-2">
-            <FavoritePlaceIconButton>
+            <FavoritePlaceIconButton @click="emit('edit')">
               <EditIcon />
             </FavoritePlaceIconButton>
-            <FavoritePlaceIconButton>
+            <FavoritePlaceIconButton @click.stop="emit('delete')">
               <DeleteIcon />
             </FavoritePlaceIconButton>
           </div>
