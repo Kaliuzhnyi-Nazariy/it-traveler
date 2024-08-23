25d7d68b0118138e5b0a8be77d5304720b25a525
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2024-08-23 23:52:42 +0300

    change link for requests

--- src/api/clientFetch.js
+++ src/api/clientFetch.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const clientFetch = axios.create({
-  baseURL: 'https://back-for-vue-js.onrender.com/',
+  baseURL: 'https://back-for-it-traveler.onrender.com/api/',
   mode: 'cors',
   Origin: '*',
   headers: {
