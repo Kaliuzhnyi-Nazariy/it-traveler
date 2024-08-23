9d909a4f495d7131153d168b3af6516c9d56997d
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2024-08-24 00:16:20 +0300

    add tokens to operations, modify operations

--- src/authService/authService.js
+++ src/authService/authService.js
@@ -1,6 +1,6 @@
 import { clientFetch } from '@/api/clientFetch'
 
-const TOKEN_KEY = 'token'
+export const TOKEN_KEY = 'token'
 
 class AuthServiceClass {
   #token = null
@@ -23,27 +23,27 @@ class AuthServiceClass {
   }
 
   async login(body) {
-    const { data } = await clientFetch.post('user/login', body)
-    const { accessToken } = data
+    const { data } = await clientFetch.post('users/login', body)
+    const { token } = data
 
-    this.setToken(accessToken)
+    this.setToken(token)
   }
 
   async register(body) {
-    const { data } = await clientFetch.post('user/register', body)
+    const { data } = await clientFetch.post('users/register', body)
     const { accessToken } = data
 
     this.setToken(accessToken)
   }
 
   async logout() {
-    await clientFetch.get('user/logout')
+    await clientFetch.get('users/logout')
 
     this.clearToken()
   }
 
   async refresh() {
-    return clientFetch.get('user/refresh')
+    return clientFetch.get('users/refresh')
   }
 }
 
