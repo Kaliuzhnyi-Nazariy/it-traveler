9a8edfd13450baaffb345856abe413debfc39375
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2024-09-06 17:26:12 +0300

    try to deploy this project

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,11 +3,16 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
+  env: {
+    browser: true, // If you're targeting the browser
+    node: true, // Enable Node.js global variables and scoping
+    es2021: true
+  },
   parserOptions: {
     ecmaVersion: 'latest'
   }
--- src/map/mapSettings.js
+++ src/map/mapSettings.js
@@ -1,9 +1,10 @@
 // import { token } from './token'
 
-import dotenv from 'dotenv'
-dotenv.config()
+// import dotenv from 'dotenv'
+// dotenv.config()
 
-const token = process.env.TOKEN
+const token = import.meta.env.VITE_TOKEN
+// const token = process.env.TOKEN
 
 export const mapSettings = {
   style: 'mapbox://styles/mapbox/streets-v12',
